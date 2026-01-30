57463013f7a6a3af49eeea9e76dcaa1b7fd7c0f8
Caden <cadenchiang@gmail.com>
2026-01-29 22:06:42 -0800

    Add Latinx Business Student Association

---
+++ supabase/migrations/20240130000038_add_lbsa.sql
@@ -0,0 +1,11 @@
+-- ADD LATINX BUSINESS STUDENT ASSOCIATION (LBSA)
+INSERT INTO clubs (name, description, category, image_url, elo_rating, total_votes, wins)
+VALUES (
+  'Latinx Business Student Association',
+  'empowering Latinx leaders through professional development, familia, academics & community service since 1998',
+  'business',
+  'https://se-images.campuslabs.com/clink/images/c4eb2f30-abef-42e2-9c6f-08959c57da0e177a9e5d-86f6-452c-baf2-0d72a89c42b6.png',
+  1500,
+  0,
+  0
+);
