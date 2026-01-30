7744b1dd139fb4841367b259684141ce22ac80cc
Caden <cadenchiang@gmail.com>
2026-01-29 22:01:20 -0800

    Add Camp Kesem Berkeley

---
+++ supabase/migrations/20240130000037_add_kesem_berkeley.sql
@@ -0,0 +1,11 @@
+-- ADD CAMP KESEM BERKELEY
+INSERT INTO clubs (name, description, category, image_url, elo_rating, total_votes, wins)
+VALUES (
+  'Camp Kesem Berkeley',
+  'student-run org providing free summer camps for kids with parents who have cancer',
+  'social-good',
+  'https://se-images-blob.campuslabs.com/communities/1660/coverphotos/06a5dbcf-a117-492e-a6b0-108bab1614d8.jpg',
+  1500,
+  0,
+  0
+);
